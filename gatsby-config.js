9b80b8fa17c06818857f8743e1f73ec2b40609c8
AdamSiekierski <adam.siekiera@outlook.com>
2020-04-18 20:00:00 +0200

    Run build workflow on contentful update

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,6 @@ module.exports = {
         ],
       },
     },
-    /* {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    }, */
     {
       resolve: `gatsby-source-contentful`,
       options: {
